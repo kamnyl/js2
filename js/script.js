039ca6e5146bf6900e22e0880657d10307c70c02
kamnyl <kamillanylund@gmail.com>
2025-03-22 15:24:16 +0100

    Added function to handle button click event and dynamically display result using document.getElementById

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 // let userInput = prompt("Enter the product price:");
 
-let price = parseFloat(userInput.replace("$",""));
+// let price = parseFloat(userInput.replace("$",""));
 
-let discount = price * 0.9;
-let discountAmount = price * 0.1;
+// let discount = price * 0.9;
+// let discountAmount = price * 0.1;
 
-console.log(`Your new price is: $${discount.toFixed(2)}`);
-console.log(`You will save: $${discountAmount.toFixed(2)}`);
+// console.log(`Your new price is: $${discount.toFixed(2)}`);
+// console.log(`You will save: $${discountAmount.toFixed(2)}`);
+
+
+
+function calculateFunction() {
+    let userInput = prompt ("Enter the product price:");
+    
+    let price = parseFloat(userInput.replace("$",""));
+
+        let discount = price * 0.9; 
+        let discountAmount = price * 0.1;
+        
+        document.getElementById("new-price").innerHTML = `Your new price is $${discount.toFixed(2)}`;
+        document.getElementById("discount-amount").innerHTML = `You will save $${discountAmount.toFixed(2)}`; 
+
+}
