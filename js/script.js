4df381d1b3137756fb2ea6f09d4613636ecb070b
kamnyl <kamillanylund@gmail.com>
2025-03-22 12:15:00 +0100

    Added JavaScript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let userInput = prompt("Enter the product price:");
+
+let price = parseFloat(userInput.replace("$",""));
+
+let discount = price * 0.9;
+let discountAmount = price * 0.1;
+
+console.log(`Your new price is: $${discount.toFixed(2)}`);
+console.log(`You will save: $${discountAmount.toFixed(2)}`);
